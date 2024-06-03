c7e38986fd74973c027e7711970a695c8ed35595
Roman <roman123457700@gmail.com>
2024-06-03 18:29:48 +0200

    modified:   css.css 	deleted:    img/Rectangle 18.jpg 	new file:   img/Rectangle p1.svg 	new file:   img/Rectangle p2.svg 	new file:   img/Rectangle p3.svg 	new file:   img/Rectangle p4.svg 	modified:   index.html 	modified:   js.js

--- js.js
+++ js.js
@@ -16,3 +16,29 @@ buttons.forEach((button, cout) => {
     paragraphs[cout].classList.toggle("classOn");
   };
 });
+
+let tabs = document.querySelectorAll(".tabs_btn");
+let img = document.querySelector(".sectionWorkImg");
+// let heading = document.querySelectorAll(".sectionWorkHeading");
+
+tabs.forEach(function (tabsBtn) {
+  tabsBtn.addEventListener("click", function (event) {
+    const path = event.currentTarget.dataset.path;
+    console.log(path);
+
+    // usuwamy clase active ze wszystkich elementów
+    document
+      .querySelectorAll(".sectionWorkContentRight")
+      .forEach(function (tabContent) {
+        console.log(9);
+        tabContent.classList.remove("paragfActive");
+      });
+
+    // dodajemy active do konkretnego elementu
+    document
+      .querySelector(`[data-target="${path}"]`)
+      .classList.add("paragfActive");
+
+    img.src = `img/Rectangle ${path}.svg`;
+  });
+});
